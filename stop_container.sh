98b99ebd15638df209ef56b826021c9c2e8f1b8e
ravishansenevirathna <travishan55@gmail.com>
2025-01-22 16:02:02 +0530

    dev codeDeploy debug commit

--- stop_container.sh
+++ stop_container.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -e
 
-# Stop the running container (if any)
+# Get the ID of the running container (if any)
 containerId=`docker ps | awk -F " " '{print $1}'`
+
+# Forcefully stop and remove the container using its ID
 docker rm -f "$containerId"
